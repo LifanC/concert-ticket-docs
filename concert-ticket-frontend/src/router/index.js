73b632db41d6b10adbdfcbc65fa48f3ebbe30bd1
陳立帆 <luke@eleadernet.com>
2026-09-16 18:22:01 +0800

    fix

--- concert-ticket-frontend/src/router/index.js
+++ concert-ticket-frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
-import { toFindCookie } from '@/components/componentsJs/Cookie'
+import { toFindCookie } from '@/components/componentsJs/cookie'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
